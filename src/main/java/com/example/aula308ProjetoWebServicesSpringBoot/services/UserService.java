e504452778b3188f0cee695922af55b4bba060a6
Matheus Proenca <matheusproencaz@gmail.com>
2022-01-18 14:21:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/example/aula308ProjetoWebServicesSpringBoot/services/UserService.java
@@ -0,0 +1,30 @@
+package com.example.aula308ProjetoWebServicesSpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aula308ProjetoWebServicesSpringBoot.entities.User;
+import com.example.aula308ProjetoWebServicesSpringBoot.repositories.UserRepository;
+
+//Component registration - para o UserService funcionar é preciso colocar uma anotação (@) antes da classe de Serviço.
+//As anotaçõe são: @Component @Repository @Service
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
