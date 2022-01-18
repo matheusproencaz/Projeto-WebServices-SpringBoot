5aaf552888e1fe5733fb1da31f6389f23cff8335
Matheus Proenca <matheusproencaz@gmail.com>
2022-01-18 12:21:20 -0300

    Projeto inciado com entidade e recurso user criada e banco de dados h2 implementado

---
+++ src/main/java/com/example/aula308ProjetoWebServicesSpringBoot/Aula308ProjetoWebServicesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula308ProjetoWebServicesSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula308ProjetoWebServicesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula308ProjetoWebServicesSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aula308ProjetoWebServicesSpringBoot/Aula308ProjetoWebServicesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula308ProjetoWebServicesSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula308ProjetoWebServicesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
